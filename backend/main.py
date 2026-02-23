d4080f1775ec0f7259c7732a73ae4ec396bb237f
Silvex <sil.the.first@gmail.com>
2026-02-23 23:18:26 +0100

    podłączenie Supabase, endpoint /notes

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import create_engine, text
+from dotenv import load_dotenv
+import os
+
+load_dotenv(r"C:\home\programowanie\fullstack\hello-fullstack\backend\.env")
 
 app = FastAPI()
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -10,6 +16,16 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+engine = create_engine(os.getenv("DATABASE_URL"))
+
 @app.get("/hello")
 def hello():
     return {"message": "Hello from FastAPI!"}
+
+@app.get("/notes")
+def get_notes():
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM notes"))
+        notes = [dict(row._mapping) for row in result]
+    return notes
+
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,26 @@
 import { useState, useEffect } from "react"
 
 function App() {
-  const [msg, setMsg] = useState("Ładowanie...")
+  const [notes, setNotes] = useState([])
 
   useEffect(() => {
-    fetch("https://hello-fullstack.onrender.com/hello")
+    fetch("http://127.0.0.1:8000/notes")
       .then(res => res.json())
-      .then(data => setMsg(data.message))
+      .then(data => setNotes(data))
   }, [])
 
-  return <h1>{msg}</h1>
+  return (
+    <div>
+      <h1>Notatki</h1>
+      {notes.map(note => (
+        <div key={note.id}>
+          <h2>{note.title}</h2>
+          <p>{note.content}</p>
+          <p>Tagi: {note.tags.join(", ")}</p>
+          <hr />
+        </div>
+      ))}
+    </div>
+  )
 }
-
 export default App
